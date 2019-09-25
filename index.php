a867f5fccc12812f20bcb1183f0104bd275494a9
karina <karinapaz2002@gmail.com>
2019-09-24 21:27:26 -0300

    Formulário de recuperação de senha
    close #2

--- index.php
+++ index.php
@@ -89,7 +89,51 @@
                         </div>
 
                 </section>
-            </main>
+                <br> 
+                <!-- Final da Sessão de Login -->
+
+                <!-- Formulário de recuperação de senha -->
+                <section class="row mt-5">
+                <div class="col-lg-4 offset-lg-4 bg-light rounded"
+                    id="caixaLogin">
+                    <h2 class="text-center mt-2">
+                        Gerar Nova Senha
+                    </h2>
+                    
+                    <form action="#" method="post" id="formSenha" class="p-2">
+                        <div class="form-group">
+                            <small class="text-muted">
+                                Para gerar uma nova senha digite seu e-mail.
+                                Click no link gerado.
+                            </small>
+
+                        </div>
+                    <div class="form-group">
+                        <input type="email" name="emailGerarSenha" id="emailGerarSenha"
+                        id="emailGerarSenha" class="form-control" placeholder="Email de recuperação de senha">
+
+
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="::Gerar::"
+                        name="btnGerar" id="btnGerar"
+                        class="btn btn-primary btn-block">
+                    </div>
+                    <div class="form-group">
+                        <p class="text-center">
+                            Já registrado? <a href="#" id="btnJaRegistrado">
+                                Entrar por aqui.
+                            </a>
+
+                        </p>
+
+                    </div>
+                </form>
+             </div>
+        </section>
+        <!-- Fim da Seção de recuperação de senha!!-->
+    </main>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
