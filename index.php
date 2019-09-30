1952f63aee408e720b59882f5566c1441b9fd578
Karina <karinapaz2002@gmail.com>
2019-09-30 19:57:30 -0300

    Criação de cadastro do Usuario
    SenhaUsuario
    E-mail
    Nome

--- index.php
+++ index.php
@@ -40,12 +40,11 @@
 
                 </h2>
     <form action="#" method="post" class="p-2" id="formLogin">
-         
+    
         <div class="form-group">
             <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário"
             class="form-control">
-        </div>
-           
+        </div> 
 
             <div class="form-group">
                 <input type="password" name="senhaUsuario"
@@ -129,11 +128,46 @@
                         </p>
 
                     </div>
-                </form>
-             </div>
+                </form>     
+        </div>
         </section>
         <!-- Fim da Seção de recuperação de senha!!-->
-    </main>
+
+<!-- Inicio do formulario de cadastro de novos usuarios -->
+        <section class= "row mt-5">
+        <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
+        <h2 class="text-center mt-2">Registre-se aqui</h2>
+        <form action="#" method="post" class="p-2" id="formRegistro">
+
+            <div class="form-group"> 
+                <input type="text" name="nomeCompleto" id="nomeCompleto" 
+                class="form-control" placeholder="Nome completo">
+            </div>
+
+            <div class="form-group">
+                <input type="text" name="nomeUsuario" id="nomeUsuario" 
+                class="form-control" placeholder="Nome de usuario">
+            </div>
+
+            <div class="form-group">
+            <input type="email" name="emailUsuario" id="emailUsuario" 
+            class="form-control" placeholder="E-mail">
+            
+            </div>
+
+            <div class="form-group">
+            <input type="password" name="senhaUsuario" id="senhaUsuario" 
+            class="form-control" placeholder="Senha">
+            </div>
+
+        </form>
+        
+        </div>
+
+        </section>
+<!-- Final do formulario de cadastro de novos usuários -->
+
+        </main>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
