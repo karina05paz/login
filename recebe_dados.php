615aafd7eb696c689d4ccc13aa48dcfca9c39b4e
karina <karinapaz2002@gmail.com>
2019-10-15 19:53:42 -0300

    Conexão com banco de dados

--- recebe_dados.php
+++ recebe_dados.php
@@ -1,35 +1,43 @@
+<?php
 
-<?php 
+require_once 'configBD.php';
+
+function verificar_entrada($entrada){
+    $saida = htmlspecialchars($entrada);
+    $saida = stripslashes ($saida);
+    $saida = trim($saida);
+
+    return $saida;//retorna a saida limpa
+    
 
 
+
+}
 //Teste se existe a ação
-if(isset($_POST['action'])){
-
-if ( $_POST['action'] =='cadastro'){
-        echo  "\n <p>cadastro</p>";
-        echo "\n<pre>";
+if (isset($_POST['action'])) {
+    if ($_POST['action'] == 'cadastro') {
+        //Teste se ação é igual a cadastro
+        echo "\n<p>cadastro</p>";
+        echo "\n<pre>"; //Pre-formatar
         print_r($_POST);
-        echo "\n</pre>";
-
-}else if($_POST['action']=='login'){
-        echo "\n <p>login</p>";
-        echo "\n<pre>";
+        echo "\n<\pre>";
+    } else if ($_POST['action'] == 'login') {
+        //Senão, teste se ação é login
+        echo "\n<p>login</p>";
+        echo "\n<pre>"; //Pre-formatar
         print_r($_POST);
-        echo"\n</pre>";
-    //Senão teste se ação é login
-
-
-}else if($_POST['action']=='senha'){
-        echo "\n <p>senha</p>";
-        echo "\n<pre>";
+        echo "\n<\pre>";
+    } else if ($_POST['action'] == 'senha') {
+        //Senão, teste se ação é recuperar senha
+        echo "\n<p>senha</p>";
+        echo "\n<pre>"; //Pre-formatar
         print_r($_POST);
-        echo "\n</pre>";
-
-    //Senão, teste se ação é recuperar senha
-
-} else{
+        echo "\n<\pre>";
+    } else {
+        header("location:index.php");
+    }
+} else {
+    //Redirecionando para index.php, negando o acesso
+    //a esse arquivo diretamente.
     header("location:index.php");
 }
-} else{
-    header("location:index.php");
-} 
--- receber.php
+++ receber.php
@@ -1,46 +1,32 @@
 <?php
 # Back-end
-
-if(isset($_GET['nome'])){
-
-echo "\n<h1>Envio de dados método <em>GET</em></h1>";
-  
-echo "\n<pre>\n";
-print_r ($_GET); #Array
-echo "\n</pre>\n";
-
-print ("\n<br><strong>Nome:</strong>");
-print("$_GET[nome]");
-
-print("\n<br><strong>email:</strong>");
-print("$_GET[email]");
-
-
-print("\n<br><strong>senha:</strong>");
-print("$_GET[senha]");
-
-print("\n<br><strong>dataDeNascimento:</strong>");
-print("$_GET[data_de_nascimento]");
-
-
+//Verifica se o método GET está enviando dados
+if (isset($_GET['nome'])) {
+    echo "\n<h1>Envio de dados método <em>GET</em></h1>";
+    echo "\n<pre>\n";
+    print_r($_GET); #Array
+    echo "\n</pre>\n";
+    print("\n<br><strong>Nome: </strong>");
+    print("$_GET[nome]");
+    print("\n<br><strong>E-mail: </strong>");
+    print("$_GET[email]");
+    print("\n<br><strong>Senha: </strong>");
+    print("$_GET[senha]");
+    print("\n<br><strong>Data de Nascimento: </strong>");
+    print("$_GET[data_de_nascimento]");
 }
-
-if(isset($_POST['nome'])){
-    echo "\n<h1>Envio de dados método <em>POST</em> </h1>";
-echo "\n<pre>\n";
-print_r ($_POST); #Array
-echo "\n</pre>\n";
-
-print ("\n<br><strong>Nome:</strong>");
-print("$_POST[nome]");
-
-print("\n<br><strong>email:</strong>");
-print("$_POST[email]");
-
-
-print("\n<br><strong>senha:</strong>");
-print("$_POST[senha]");
-
-print("\n<br><strong>dataDeNascimento:</strong>");
-print("$_POST[data_de_nascimento]");
+//Verifica se o método POST está enviando dados
+if (isset($_POST['nome'])) {
+    echo "\n<h1>Envio de dados método <em>POST</em></h1>";
+    echo "\n<pre>\n";
+    print_r($_POST); #Array
+    echo "\n</pre>\n";
+    print("\n<br><strong>Nome: </strong>");
+    print("$_POST[nome]");
+    print("\n<br><strong>E-mail: </strong>");
+    print("$_POST[email]");
+    print("\n<br><strong>Senha: </strong>");
+    print("$_POST[senha]");
+    print("\n<br><strong>Data de Nascimento: </strong>");
+    print("$_POST[data_de_nascimento]");
 }
